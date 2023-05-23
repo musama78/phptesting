09ae832f407ba0b65dcaaddeafa2de3fe584b631
musama78 <muhammadusama7@yahoo.com>
2023-05-23 19:30:41 +0000

    variable

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 </head>
 <body>
 
-    <?php echo "<h1>Hi! I'm Usama, This is SCM auto pull + image push... 12.27</h1>"; ?>
+    <?php echo "<h1>Hi! I'm Usama, This is SCM auto pull + image push... 12.30</h1>"; ?>
     <?php echo "<h3>hi This is GIthub active and k8s Class!</h3>"; ?>
 
 </body>
